4c561e598ed71feccc0b66a7a6a2dd4b2efcfeb9
Julius Rückin <julius.rueckin@rats-os.de>
2015-05-16 01:17:54 +0200

    added opportunity to print trainings table as a bill on group page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,22 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function printTrainingTable(){
+	var restorePage = document.body.innerHTML;
+	$("#trainings_table").find("td").each(function(){
+		if($(this).text().trim() == "Training löschen"){
+			$(this).html("");
+		}
+	});
+	$("#trainings_table").find("th").each(function(){
+		if($(this).text().trim() == "Aktionen"){
+			$(this).html("");
+		}
+	});
+	var printContent = document.getElementById("trainings_table").outerHTML;
+	document.body.innerHTML = printContent;
+	window.print();
+	document.body.innerHTML = restorePage;
+}
