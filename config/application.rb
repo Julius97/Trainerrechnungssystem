2d2ceef579adb45ccda5c93d571028525df0728c
Julius Rückin <julius.rueckin@rats-os.de>
2015-05-17 18:57:53 +0200

    small mobile design and timezone bug fix

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module Rainerrechnungssystem
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    #config.time_zone = "Europe/Berlin"
+    Time.zone = "Berlin"
     config.assets.precompile += %w( mobile.css )
   end
 end
