d7aa84bc83c3c355d5290a53239977646fc3aac8
Julius Rückin <julius.rueckin@rats-os.de>
2015-05-18 02:21:48 +0200

    fixed date and time tile time zone problem

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,13 +8,13 @@ class DashboardController < ApplicationController
 
 	def time_status
 		time_status_str = ""
-		time_status_str = DateTime.now.strftime("%H:%M")
+		time_status_str = Time.now.in_time_zone(Time.zone.name).strftime("%H:%M")
 		render :html => time_status_str
 	end
 
 	def date_status
 		date_status_str = ""
-		date_status_str = DateTime.now.strftime("%a, %B %y")
+		date_status_str = Time.now.in_time_zone(Time.zone.name).strftime("%a, %B %y")
 		render :html => date_status_str
 	end
 	
